3abe3306a95a63f148c94c826e803e0ed4650efe
Sourabha Jena <sourabhajena2003@gmail.com>
2026-08-27 01:12:59 +0530

    condition and function

---
+++ JavaSript/Lecture_30/conditional.js
@@ -0,0 +1,30 @@
+console.log("Welcome back Sourabha!");
+
+// let num = 43;
+// // console.log(num++); 
+// console.log(++num);
+
+// const age = 19;
+// const hasId = true;
+// const canEnterClub  =  age >= 18 && hasId ===true;
+// console.log(canEnterClub);
+
+// function add(a, b){
+//     console.log(a + b);
+// }
+// let a = 10;
+// b = 20;
+// add(a,b);
+
+
+function marks(name,mark_1,mark_2,mark_3){
+    // console.log(`${name} marks:\nEnglish: ${mark_1}\nMath: ${mark_2}\nScience: ${mark_3}`);
+    console.log(`${name} marks:--- English: ${mark_1}  Math: ${mark_2}  Science: ${mark_3}`);
+
+     
+}
+
+const Sourabha = marks("Sourabha",98,87,93);
+const puja = marks("Puja",98,90,93);
+const kohil = marks("Sourabha",90,87,83);
+const david = marks("Sourabha",98,89,90);
