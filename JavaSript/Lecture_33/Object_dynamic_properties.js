3dac64a7b8bf599a64e59928505a2032511fbea1
Sourabha Jena <sourabhajena2003@gmail.com>
2026-08-27 04:02:43 +0530

    Object_dynamic_properties.js

---
+++ JavaSript/Lecture_33/Object_dynamic_properties.js
@@ -0,0 +1,9 @@
+let students = [
+    { name: "Rahul", age: 20 },
+    { name: "Amit", age: 21 },
+    { name: "Sourabha", age: 23 }
+];
+
+console.log(students);
+console.log(students[0]);
+console.log(students[2].age);
