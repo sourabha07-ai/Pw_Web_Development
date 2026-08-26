89e2e8ab8b34bfc91fd3c2419ded03c61e2694a3
Sourabha Jena <sourabhajena2003@gmail.com>
2026-08-26 08:18:27 +0530

    index.js update

--- JavaSript/Lecture_29/index.js
+++ JavaSript/Lecture_29/index.js
@@ -11,22 +11,22 @@
  
 
 //! let 
-let name = "Sourabha!";
-console.log(name);
-// let name = "liku";  //redeclaration allowed
-name = "Sourabha Bhai! ";
-console.log(name);
+// let name = "Sourabha!";
+// console.log(name);
+// // let name = "liku";  //redeclaration allowed
+// name = "Sourabha Bhai! ";
+// console.log(name);
 
 //! const 
-// const adharCard = 1234567;
-// console.log(adharCard);
+const adharCard = 1234567;
+console.log(adharCard);
 // const adharCard  = 32456; //Re declaration not allowed
 // adharCard = 325643; //TypeError: Assignment to constant variable.
-// console.log(adharCard);
+console.log(adharCard);
 
 //! crezy 
-const name_Of_Vip = "Sourabha!";
-const message = `Welcome back ${name_Of_Vip}`;
-console.log(message);
+// const name_Of_Vip = "Sourabha!";
+// const message = `Welcome back ${name_Of_Vip}`;
+// console.log(message);
 
 
