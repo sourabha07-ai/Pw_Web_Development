7324d81c4372c778f7595ec0fb19c2f6c1c8c9f2
Sourabha Jena <sourabhajena2003@gmail.com>
2026-08-29 23:00:24 +0530

    all

--- JavaSript/Lecture_33_01/for_Loop.js
+++ JavaSript/Lecture_33_01/for_Loop.js
@@ -1,7 +1,16 @@
 // Traditional for loop ⭐⭐⭐
-let arr = [10, 20, 30,40,50];
+//  Function + Array
+//   Suppose we have an array of marks and want to print every mark.
 
-for (let i = 0; i < 5; i++) {
-    console.log(i);
+function studentMarks(){
+    for(let i = 0; i < marks.length;i++){
+        console.log(marks[i]);
+    }
 }
 
+let marks = [98,78,90,80,90];
+studentMarks(marks);
+
+// for(let i = 0; i < marks.length;i++){
+//     console.log(marks[i]);
+// }
