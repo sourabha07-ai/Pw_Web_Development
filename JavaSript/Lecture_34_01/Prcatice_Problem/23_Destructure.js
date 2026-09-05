cd4139c2994bd6d0cb3ec8f4ee1d56e39f6ad3c2
Sourabha Jena <sourabhajena2003@gmail.com>
2026-09-05 15:28:28 +0530

    24_array.js

---
+++ JavaSript/Lecture_34_01/Prcatice_Problem/23_Destructure.js
@@ -0,0 +1,11 @@
+let student = {
+    name: "Sourabha",
+    age: 23,
+    course: "MCA"
+};
+
+let {name,age, course} = student;
+console.log(student.name);
+console.log(name);
+console.log(age);
+console.log(course);
---
+++ JavaSript/Lecture_34_01/Prcatice_Problem/24_array.js
@@ -0,0 +1,5 @@
+let arr = [1,2,3,45];
+
+let [a,b,c,d] = arr;
+
+console.log(a,b,c,d);
