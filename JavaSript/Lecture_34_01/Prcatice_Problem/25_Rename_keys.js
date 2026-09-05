e51baae441bfd0c38c76f8cdae0953c6bd81bcf3
Sourabha Jena <sourabhajena2003@gmail.com>
2026-09-05 15:35:46 +0530

    spreed operator

---
+++ JavaSript/Lecture_34_01/Prcatice_Problem/25_Rename_keys.js
@@ -0,0 +1,9 @@
+let student = {
+    name: "Sourabha",
+    age: 23
+};
+
+let {name : student_name, age : student_Age} = student;
+console.log(student_name);
+console.log(student_Age);
+console.log(student);
