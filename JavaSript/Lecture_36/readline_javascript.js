6b7a9527d2a1f0c599eeba852d276e7f3c9b5538
Sourabha Jena <sourabhajena2003@gmail.com>
2026-09-06 08:56:08 +0530

    add 35 and 36 file

---
+++ JavaSript/Lecture_36/readline_javascript.js
@@ -0,0 +1,11 @@
+let readline = require("readline");
+
+const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+});
+
+rl.question('what is your name: ',(name)=>{
+        console.log(`Hello: ${name}`);
+        rl.close();
+});
