5bcd5bc4caaa9e95b66d32ee920d377038a2be75
Sourabha Jena <sourabhajena2003@gmail.com>
2026-09-06 16:09:30 +0530

    Templete literal

---
+++ JavaSript/Lecture_35_String/19_Templet_literal.js
@@ -0,0 +1,17 @@
+let name = "Sourabha!";
+
+let message = `Hello!...${name}`;
+console.log(message);
+
+
+let name2 = "Sourabha";
+let age1 = 23;
+
+console.log("My name is " + name2 + " and I am " + age1 + " years old.");
+
+
+
+let name3 = "Sourabha";
+let age2 = 24;
+
+console.log(`My name is ${name3} and I am ${age2} years old.`);
---
+++ JavaSript/Lecture_35_String/21_multiLIne_String.js
@@ -0,0 +1,7 @@
+let message = `
+Hello Sourabha!
+What are you doing right now ?
+Can you talk to me ?
+`;
+
+console.log(message);
