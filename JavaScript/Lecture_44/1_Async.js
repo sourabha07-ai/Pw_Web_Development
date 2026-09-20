3ab48158786da7bf3591eb0b386814547c7542e9
Sourabha Jena <sourabhajena2003@gmail.com>
2026-09-20 19:43:07 +0530

    Project and Github file

---
+++ JavaScript/Lecture_44/1_Async.js
@@ -0,0 +1,16 @@
+console.log("Hello");
+
+setTimeout(() => {
+   console.log("1 setTimeout");
+   
+}, 3000);
+
+setTimeout(()=>{
+   console.log("2 setTimeout");
+},1000);
+
+setTimeout(()=>{
+   console.log("3 setTimeout");
+},2000);
+
+console.log("Sourabha");K
