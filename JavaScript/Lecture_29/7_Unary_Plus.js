7390c856f4adcf26ad35150dc89335459f465f4b
Sourabha Jena <sourabhajena2003@gmail.com>
2026-09-20 11:36:36 +0530

    7_Unary_Plus.js

---
+++ JavaScript/Lecture_29/7_Unary_Plus.js
@@ -0,0 +1,12 @@
+// let value = '50';
+
+// let num1 = Number(value); // 50
+// let num2 = +value; // 50
+
+// console.log(num1);
+// console.log(num2);
+
+
+// let str = "234A6";
+// console.log(+str);
+
