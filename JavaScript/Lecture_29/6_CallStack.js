a002c3a0dea59e44db8d7065d226a068f86119d1
Sourabha Jena <sourabhajena2003@gmail.com>
2026-09-20 11:27:44 +0530

    6_CallStack.js

---
+++ JavaScript/Lecture_29/6_CallStack.js
@@ -0,0 +1,13 @@
+function greeting() {
+  // [1] Some code here
+  sayHi();
+  // [2] Some code here
+}
+function sayHi() {
+  return console.log("Hi!");
+}
+
+// Invoke the `greeting` function
+greeting();
+
+// [3] Some code here
